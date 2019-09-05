0c53c5dc8df0d7307b76da1cd5d4c3e12d6b077e
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-09-05 01:37:49 +0100

    Added brief demonstration of sorting method into Main.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        List<PokerHand> handList = new ArrayList<>();
+
+        handList.add(new PokerHand("AH 7C AD 10C 10S"));
+        handList.add(new PokerHand("10C 10S 10D 4D 4C"));
+        handList.add(new PokerHand("7S 7C 3S 3D 9H"));
+        handList.add(new PokerHand("5D 6D 7D 8D 9D"));
+        handList.add(new PokerHand("10H JH QH KH AH"));
+
+        Collections.sort(handList);
+
+        for(PokerHand hand : handList){
+
+            System.out.println(hand.getUserDescription() + " is a " + hand.getClassification());
+
+        }
+
+
 
     }
 }
